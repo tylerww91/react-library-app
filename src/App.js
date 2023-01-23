cfcf3ec1cd06dd526dd3fc745a250fa8b98c86d5
Tyler Watson <tylerww91@gmail.com>
2023-01-23 15:11:00 -0800

    builds /books/:id route which returns a detail of a book that matches the id

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
@@ -8,6 +9,7 @@ function App() {
     <main className="container">
       <h1>Library Catalog</h1>
       <Switch>
+        <Route path="/books/:id" component={BookDetail} />
         <Route path="/books" component={BookList} />
         {/* <BookList /> */}
       </Switch>
