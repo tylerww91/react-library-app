e67884d73693f3aae77d0cfb64113d9f7c8b79b7
Tyler Watson <tylerww91@gmail.com>
2023-01-23 14:54:39 -0800

    builds /books route which returns a list of books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <Switch>
+        <Route path="/books" component={BookList} />
+        {/* <BookList /> */}
+      </Switch>
     </main>
   );
 }
