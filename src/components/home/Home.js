0089e9a3dceda793b50a09b7518849cac34e9d37
Tyler Watson <tylerww91@gmail.com>
2023-01-23 16:32:25 -0800

    PLEASE WORK

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,10 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      <h1>Library Catalog</h1>
       <h2>
-        Welcome to Tylers library! Check out my excellent selection of reading material within our{' '}
-        <Link to={`/books`}>catalog</Link>
+        Welcome to Tylers Library Catalog Check out my excellent selection of reading material
+        within our <Link to={`/books`}>catalog</Link>
       </h2>
     </div>
   );
